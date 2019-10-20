674a2f3ba1c1086e1ea71d33e763f43877504ff6
Igor <igor.ogerchuk@gmail.com>
2019-10-21 00:52:54 +0300

    Updates

---
+++ precache-manifest.b92e94aa19f81e4385dc44c6cd2daf78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84e878acc1bd7f25f2039112c22301bc",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "fbc5cd7f5c39c06ea1b4",
+    "url": "/goit-react-hw-01-components/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "e9942a06f28c93eef738",
+    "url": "/goit-react-hw-01-components/static/css/main.05686db0.chunk.css"
+  },
+  {
+    "revision": "fbc5cd7f5c39c06ea1b4",
+    "url": "/goit-react-hw-01-components/static/js/2.20123783.chunk.js"
+  },
+  {
+    "revision": "e9942a06f28c93eef738",
+    "url": "/goit-react-hw-01-components/static/js/main.2754797a.chunk.js"
+  },
+  {
+    "revision": "4d9ce36fabe79d32c0ed",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.7a5c0bc9.js"
+  }
+]);
